4ad9f84b96a3558fdbcc83dee3a1f2888f7df1a1
s-chulkin <chulkin163@gmail.com>
2026-05-10 18:26:38 +0200

    Styles fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,6 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-
         <!-- Styles -->
         @livewireStyles
     </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,6 @@
             </style>
         @endif
 
-        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-
     </head>
     <body class="bg-[#FDFDFC] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
