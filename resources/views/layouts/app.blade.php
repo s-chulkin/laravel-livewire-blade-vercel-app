9780238f2ed5d5a616845cd87991b514ef29dd5f
s-chulkin <chulkin163@gmail.com>
2026-05-10 17:52:15 +0200

    Tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
         <!-- Styles -->
         @livewireStyles
     </head>
